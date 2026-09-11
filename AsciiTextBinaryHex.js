c0b2bcd8cf37b6fe34bc71cc3f24c0696c27373f
bananamaninpanama <Dehstriplets@gmail.com>
2026-09-11 08:24:13 -0400

    Update AsciiTextBinaryHex.js
    I just learned there is a way to convert between bases without custom functions in javascript T-T

--- AsciiTextBinaryHex.js
+++ AsciiTextBinaryHex.js
@@ -67,31 +67,15 @@ class MyExtensionthing {
             }]
         };
     }
-    //i think this is the right way to do this thing
-    HEXBINMAP = {'0':'0000','1':'0001','2':'0010','3':'0011','4':'0100','5':'0101','6':'0110','7':'0111','8':'1000','9':'1001','A':'1010','B':'1011','C':'1100','D':'1101','E':'1110','F':'1111','a':'1010','b':'1011','c':'1100','d':'1101','e':'1110','f':'1111'}
-    BINHEXMAP = Object.fromEntries(Object.entries(this.HEXBINMAP).map(([key, value]) => [value, key]));
-    turnHexDigToDec(digittoconvert) {
-      if (args["Txt"] != ""){
-        if (/^\d+$/.test(digittoconvert)) {
-          return(Number(digittoconvert));
-        } else if (/[a-fA-F]/.test(digittoconvert)) {
-          return((((['A','B','C','D','E','F','a','b','c','d','e','f'].indexOf(digittoconvert)) % 6) + 10));
-        } else {
-          throw new Error("you put the wrong character in the function, you silly goose");
-        } 
-      } else {
-        return '';
-      }
-    }
     async Hex2String(args) {
-      if (args["Txt"] != ""){
-        let hex = args["Hex"].trim();
+    let hex = args["Hex"].trim();
+      if (hex != ""){
         if (!/^\s*([0-9a-fA-F]{2}(\s|$))*$/.test(hex)) {
           return "INVALID";
         } else {
           var banana = '';
           for (let indexthing of (hex.split(" "))) {
-            banana = `${banana}` + String.fromCharCode((this.turnHexDigToDec(indexthing.charAt(0)) * 16) + this.turnHexDigToDec(indexthing.charAt(1)));
+            banana = `${banana}` + String.fromCharCode(parseInt(indexthing, 16));
           }
           return banana;
         }
@@ -100,36 +84,30 @@ class MyExtensionthing {
       }
     }
     async Hex2Bin(args) {
-      if (args["Txt"] != ""){
-        let hex = args["Hex"].trim();
+      let hex = args["Hex"].trim();
+      if (hex != ""){
           if (!/^\s*([0-9a-fA-F]{2}(\s|$))*$/.test(hex)) {
           return "INVALID";
           } else {
             var banana = '';
             for (let indexthing of (hex.split(" "))) {
-              banana = `${banana} ` + this.HEXBINMAP[indexthing.charAt(0)] + this.HEXBINMAP[indexthing.charAt(1)];
+              banana = `${banana} ` + ((parseInt(indexthing, 16)).toString(2)).padStart(8, '0');
             }
             return (banana.trim());
-        }
+          }
       } else {
         return '';
       }
     }
     async Bin2String(args) {
-      if (args["Txt"] != ""){
-        let bin = args["Bin"].trim();
+      let bin = args["Bin"].trim();
+      if (bin != ""){
         if (!/^\s*([0-1]{8}(\s|$))*$/.test(bin)) {
           return "INVALID";
           } else {
             var banana = '';
             for (let indexthing of (bin.split(" "))) {
-              var idonwannakeepmakingvariablenamessssss = 0;
-              for (let [manin, panama] of indexthing.split("").entries()) {
-                if (panama === '1') {
-                  idonwannakeepmakingvariablenamessssss += (2 ** (7 - manin));
-                }
-              }
-              banana = `${banana}` + String.fromCharCode(idonwannakeepmakingvariablenamessssss);
+              banana = `${banana}` + String.fromCharCode(parseInt(indexthing, 2));
             }
             return banana;
           }
@@ -138,14 +116,14 @@ class MyExtensionthing {
       }
     }
     async Bin2Hex(args) {
-      if (args["Txt"] != ""){
-        let bin = args["Bin"].trim();
+      let bin = args["Bin"].trim();
+      if (bin != ""){
         if (!/^\s*([0-1]{8}(\s|$))*$/.test(bin)) {
           return "INVALID";
         } else {
           var banana = '';
           for (let indexthing of (bin.split(" "))) {
-            banana = `${banana} ` + this.BINHEXMAP[indexthing.substring(0,4)] + this.BINHEXMAP[indexthing.substring(4,8)];
+            banana = `${banana} ` + ((parseInt(indexthing, 2)).toString(16)).padStart(2, '0');
           }
           return (banana.trim());
         }
@@ -154,24 +132,11 @@ class MyExtensionthing {
       }
     }
     async String2Hex(args) {
-      if (args["Txt"] != ""){
+      var txt = args["Txt"];
+      if (txt != ""){
         var banana = '';
-        for (let indexthing of args["Txt"].split("")) {
-          var OOOOOHHH = '';
-          //this is a donkey kong refrence
-          var AHHHHHHHHHHHH = indexthing.charCodeAt();
-          //why must coding be so tedious
-          for (let ENDMYSUFFERINGPLEASE = 0; ENDMYSUFFERINGPLEASE < 8; ENDMYSUFFERINGPLEASE++) {
-            if (AHHHHHHHHHHHH % 2 === 1) {
-            OOOOOHHH = 1 + `${OOOOOHHH}`;
-            AHHHHHHHHHHHH = (AHHHHHHHHHHHH - 1) / 2;
-            } else {
-            OOOOOHHH = 0 + `${OOOOOHHH}`;
-            AHHHHHHHHHHHH = (AHHHHHHHHHHHH) / 2;
-            }
-            //this isn't confusing at all
-          }
-          banana = `${banana} ` + this.BINHEXMAP[OOOOOHHH.substring(0,4)] + this.BINHEXMAP[OOOOOHHH.substring(4,8)];
+        for (let indexthing of txt.split("")) {
+          banana = `${banana} ` + `${((indexthing.charCodeAt()).toString(16)).padStart(2, '0')}`;
         }
         return (banana.trim());
       } else {
@@ -179,24 +144,11 @@ class MyExtensionthing {
       }
     }
     async String2Bin(args) {
-      if (args["Txt"] != ""){
+      var txt = args["Txt"];
+      if (txt != ""){
         var orange = '';
-        for (let indexthing of args["Txt"].split("")) {
-          var OOOOOHHH = '';
-          //this is a donkey kong refrence
-          var AHHHHHHHHHHHH = indexthing.charCodeAt();
-          //why must coding be so tedious
-          for (let ENDMYSUFFERINGPLEASE = 0; ENDMYSUFFERINGPLEASE < 8; ENDMYSUFFERINGPLEASE++) {
-            if (AHHHHHHHHHHHH % 2 === 1) {
-            OOOOOHHH = 1 + `${OOOOOHHH}`;
-            AHHHHHHHHHHHH = (AHHHHHHHHHHHH - 1) / 2;
-            } else {
-            OOOOOHHH = 0 + `${OOOOOHHH}`;
-            AHHHHHHHHHHHH = (AHHHHHHHHHHHH) / 2;
-            }
-            //this isn't confusing at all
-          }
-        orange = `${orange} ` + `${OOOOOHHH}`;
+        for (let indexthing of txt.split("")) {
+          orange = `${orange} ` + `${((indexthing.charCodeAt()).toString(2)).padStart(8, '0')}`;
         }
         return (orange.trim());
       } else {
