ba9d76586fe485e030937057746ca895b6dff383
bananamaninpanama <Dehstriplets@gmail.com>
2026-09-13 14:16:47 -0400

    Update agBuffer-BIP.js
    lets just seeeeeeee

--- Extensions/ArrayBuffersExtension-MyVersion/agBuffer-BIP.js
+++ Extensions/ArrayBuffersExtension-MyVersion/agBuffer-BIP.js
@@ -449,6 +449,7 @@
         getSTRINGValue() {
             try {
                 if (this.buffer.arrayBuffer === new Uint8Array(16)) return "the arraybuffer IS EQUAL TO 'new Uint8Array(16)'";
+                if (this.buffer.arrayBuffer.byteLength === 16) return "the arraybuffer's length IS EQUAL TO '16' (number)";
                 let banana = this.buffer.getSTRING(this.type,this.index % this.buffer.arrayBuffer.byteLength,this.endian)
                 if (banana === new Uint8Array(0)) return "IS EQUAL TO 'new Uint8Array(0)'";
                 if (banana === new Uint8Array(1)) return "IS EQUAL TO 'new Uint8Array(1)'";
