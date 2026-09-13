46f3b99263bab6542228b97cb817e3ad00a7cccd
bananamaninpanama <Dehstriplets@gmail.com>
2026-09-13 13:16:32 -0400

    Update agBuffer-BIP.js
    WILL THIS WORK???

--- Extensions/ArrayBuffersExtension-MyVersion/agBuffer-BIP.js
+++ Extensions/ArrayBuffersExtension-MyVersion/agBuffer-BIP.js
@@ -2242,7 +2242,7 @@
             return PTR.endian
         }
         getStringFromPointer({PTR}) {
-            if (!PTR || !(PTR instanceof ArrayBufferPointerType)) return "";
+            if (!PTR || !(PTR instanceof ArrayBufferPointerType)) return 0;
             return PTR.getSTRINGValue()
         }
 
